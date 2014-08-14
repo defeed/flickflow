9b84ecbc34a3e552b6239a3e44c16b018362f540
Artem Pakk <apakk@me.com>
2014-08-15 02:07:14 +0700

    Show movie count on movies/index

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,6 +1,7 @@
 class Admin::MoviesController < AdminController
   def index
     @movies = Movie.with_title.paginate(page: params[:page], per_page: 72).order('released_on DESC')
+    @movie_count = @movies.count
   end
   
   def show
