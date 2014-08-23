7638fd27303dc2f800f6e80daa3568eda31bd7b6
Artem Pakk <apakk@me.com>
2014-08-23 19:27:27 +0700

    Move API out of api subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -6,33 +6,31 @@ Rails.application.routes.draw do
     root 'dashboard#index'
   end
   
-  constraints subdomain: 'api' do
-    namespace :api, path: '/' do
-      resources :movies, only: [:index, :show] do
-        with_options only: :index do |list_only|
-          list_only.resources :alternative_titles
-          list_only.resources :credits
-          list_only.resources :critic_reviews
-          list_only.resources :genres
-          list_only.resources :keywords
-          list_only.resources :posters
-          list_only.resources :recommended_movies
-          list_only.resources :releases
-          list_only.resources :trivia
-        end
+  namespace :api do
+    resources :movies, only: [:index, :show] do
+      with_options only: :index do |list_only|
+        list_only.resources :alternative_titles
+        list_only.resources :credits
+        list_only.resources :critic_reviews
+        list_only.resources :genres
+        list_only.resources :keywords
+        list_only.resources :posters
+        list_only.resources :recommended_movies
+        list_only.resources :releases
+        list_only.resources :trivia
       end
-      
-      resources :people, only: :show do
-        resources :movie_credits, only: :index
-      end
-      
-      resources :genres, only: [:index, :show] do
-        resources :movies, only: :index
-      end
-      
-      resources :keywords, only: [:index, :show] do
-        resources :movies, only: :index
-      end
+    end
+    
+    resources :people, only: :show do
+      resources :movie_credits, only: :index
+    end
+    
+    resources :genres, only: [:index, :show] do
+      resources :movies, only: :index
+    end
+    
+    resources :keywords, only: [:index, :show] do
+      resources :movies, only: :index
     end
   end
   
