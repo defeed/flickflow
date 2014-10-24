d93e8118aee01f873d0cf34165124b8cd6e6cc00
Artem Pakk <apakk@me.com>
2014-10-24 20:47:00 +0300

    Don't use asset_path helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     primary_poster = movie.primary_poster
     file_url = primary_poster.file_url(version) if primary_poster
     src = file_url.presence || "#{version}_no_poster.png"
-    image_tag asset_path(src), options
+    image_tag src, options
   end
   
   def format_runtime minutes
