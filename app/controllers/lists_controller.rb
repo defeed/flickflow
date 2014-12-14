b6cdea22c123b7a30a96c5b37cbb7a302ebe0f1e
Artem Pakk <apakk@me.com>
2014-12-14 20:43:56 +0200

    Add find_user_lists method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,12 @@
 class ListsController < ApplicationController
   def show
+    find_user_lists
     @list = current_user.lists.friendly.find(params[:id])
-    @watchlist = current_user.lists.friendly.find('watchlist')
-    @watched   = current_user.lists.friendly.find('watched')
 
     @movies = @list.movies.with_title
               .includes(:genres)
               .includes(:primary_poster)
               .order('released_on DESC')
               .paginate(page: params[:page], per_page: 42)
-    @watchlist_movies = @watchlist.movies.map(&:id)
-    @watched_movies = @watched.movies.map(&:id)
   end
 end
